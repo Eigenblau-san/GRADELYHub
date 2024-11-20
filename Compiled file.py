4ea9d2aa7ecd1fa297f815ad6f1c3519e432797c
Binsung <23ur0274@psu.edu.ph>
2024-11-20 19:52:06 +0800

    Add files via upload

---
+++ Compiled file.py
@@ -0,0 +1,11 @@
+import STUDENT_MGMT
+import ADD_SUBJ
+import COMPUTE_GRADE_LEC
+import COMPUTE_LAB
+import LecLabGradeDraft
+
+STUDENT_MGMT.main()
+ADD_SUBJ.main()
+COMPUTE_GRADE_LEC.main()
+COMPUTE_LAB.main()
+LecLabGradeDraft.main()
